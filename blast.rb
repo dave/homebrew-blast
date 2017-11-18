e956371e33a952fbb85f9c817475302014ace669
Dave Brophy <dave@brophy.uk>
2017-11-18 11:54:38 -0800

    blast version 2.0.8

--- blast.rb
+++ blast.rb
@@ -1,9 +1,9 @@
 class Blast < Formula
   desc ""
   homepage ""
-  url "https://github.com/dave/blast/releases/download/2.0.7/blast_2.0.7_darwin_amd64.tar.gz"
-  version "2.0.7"
-  sha256 "6578f498dd6f5264e3f500b793c0e40e03ce99aaec8ea4eb2e8b073fb87396f9"
+  url "https://github.com/dave/blast/releases/download/2.0.8/blast_2.0.8_darwin_amd64.tar.gz"
+  version "2.0.8"
+  sha256 "5e5f52432c4937815d838eb2254bc9225a378d936bcc8b902241e086530d46af"
 
   def install
     bin.install "blast"
